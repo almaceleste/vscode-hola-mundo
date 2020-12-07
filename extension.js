a968e172e9fc9a35824633da90d4f68b89b6f0bb
almaceleste <pcaptanovska@gmail.com>
2020-12-07 10:19:39 +0300

    fixed: exports

--- extension.js
+++ extension.js
@@ -26,7 +26,7 @@ function activate(context) {
 
 	context.subscriptions.push(disposable);
 }
-exports.activate = activate;
+// exports.activate = activate;
 
 // this method is called when your extension is deactivated
 function deactivate() {}
